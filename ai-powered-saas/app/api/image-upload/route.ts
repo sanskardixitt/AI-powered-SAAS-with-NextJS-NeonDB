3680c12d643e96dbd9d9385ab2d0a132b1951cd3
sanskardixitt <143865710+sanskardixitt@users.noreply.github.com>
2025-09-29 23:57:14 +0530

    cloudinary_setup

---
+++ ai-powered-saas/app/api/image-upload/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
+import { auth } from "@clerk/nextjs/server";
+
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDNINARY_API_SECRET,
+});
+
+interface CloudinaryUploadResult {
+  public_id: string;
+  [key: string]: any;
+}
